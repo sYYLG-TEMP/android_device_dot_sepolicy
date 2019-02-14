8a57cef1a122f22196de3c4bdb90f4b4cd850aa3
dianlujitao <dianlujitao@lineageos.org>
2019-02-14 20:53:36 +0800

    sepolicy: qcom: Rename common to vendor to avoid confusion
     * Remove platform specific rule from sepolicy.mk as well, it's never
       ever a valid usecase.
    
    Change-Id: I8bf6af30407d55beb9b2c5a2afaca091564e1605
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -3,9 +3,20 @@
 # that inherit from dotOS
 #
 
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/dot/sepolicy/qcom/private
 
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/dot/sepolicy/qcom/dynamic
+else
 BOARD_SEPOLICY_DIRS += \
-    device/dot/sepolicy/qcom/common \
-    device/dot/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/dot/sepolicy/qcom/dynamic \
+    device/dot/sepolicy/qcom/vendor
+endif
